8bbbb746db5a8133f67157f3682517409535c705
Sarthak <sarthak242@gmail.com>
2021-03-12 12:41:03 +0530

    Feat: added open and close functionality to popop

--- index.js
+++ index.js
@@ -3,3 +3,23 @@
 //         event.target.classList.toggle("active")
 //     }
 // })
+
+window.onload = () => {
+    document.querySelector("#popup__open").onclick = () => {
+        document.querySelector("#popup").classList.add("active");
+        let closeButtons = document.querySelectorAll("#popup .popup__close");
+        for(let button of closeButtons) {
+            button.addEventListener('click', closePopup);
+        }
+    }
+
+    function closePopup() {
+        let closeButtons = document.querySelectorAll("#popup .popup__close");
+        for(let button of closeButtons)
+            button.removeEventListener('click', closePopup);
+        document.querySelector("#popup").classList.remove("active");
+    }
+}
+
+
+
