8d780dfc79b2d4baa773e71561ef888f20740093
Sarthak <sarthak242@gmail.com>
2021-03-12 12:55:48 +0530

    Feat: added popups large and small with functions

--- index.js
+++ index.js
@@ -5,20 +5,50 @@
 // })
 
 window.onload = () => {
-    document.querySelector("#popup__open").onclick = () => {
-        document.querySelector("#popup").classList.add("active");
-        let closeButtons = document.querySelectorAll("#popup .popup__close");
+    document.querySelector("#small-popup-open-btn").onclick = () => {
+        document.querySelector("#small-popup").classList.add("active");
+        let closeButtons = document.querySelectorAll("#small-popup .popup__close");
         for(let button of closeButtons) {
-            button.addEventListener('click', closePopup);
+            button.addEventListener('click', closePopup.bind(null, "small-popup"));
         }
     }
 
-    function closePopup() {
-        let closeButtons = document.querySelectorAll("#popup .popup__close");
+    function closePopup(id) {
+        let closeButtons = document.querySelectorAll(`#${id} .popup__close`);
         for(let button of closeButtons)
             button.removeEventListener('click', closePopup);
-        document.querySelector("#popup").classList.remove("active");
+        document.querySelector(`#${id}`).classList.remove("active");
     }
+
+    document.querySelector("#popup-open-btn").onclick = () => {
+        document.querySelector("#popup").classList.add("active");
+        let closeButtons = document.querySelectorAll("#popup .popup__close");
+        for(let button of closeButtons) {
+            button.addEventListener('click', closePopup.bind(null, "popup"));
+        }
+    }
+
+    // function closePopup() {
+    //     let closeButtons = document.querySelectorAll("#popup .popup__close");
+    //     for(let button of closeButtons)
+    //         button.removeEventListener('click', closePopup);
+    //     document.querySelector("#popup").classList.remove("active");
+    // }
+
+    document.querySelector("#large-popup-open-btn").onclick = () => {
+        document.querySelector("#large-popup").classList.add("active");
+        let closeButtons = document.querySelectorAll("#large-popup .popup__close");
+        for(let button of closeButtons) {
+            button.addEventListener('click', closePopup.bind(null, "large-popup"));
+        }
+    }
+
+    // function closePopup() {
+    //     let closeButtons = document.querySelectorAll("#large-popup .popup__close");
+    //     for(let button of closeButtons)
+    //         button.removeEventListener('click', closePopup);
+    //     document.querySelector("#large-popup").classList.remove("active");
+    // }
 }
 
 
