143c42c5f78ceabf9cbcaaafe49471b5c72be8d7
Sarthak <sarthak242@gmail.com>
2021-03-23 17:22:20 +0530

    Feat: added code for getting-started and layout-primitives

---
+++ docs.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+    const allCodes = document.getElementsByClassName('code');
+    Array.from( allCodes ).forEach( elem => {
+        CodeMirror.fromTextArea(elem, {
+            mode: 'xml',
+            theme: 'material-darker',
+            lineNumbers: true,
+            readOnly: true
+        });    
+    })
+}
